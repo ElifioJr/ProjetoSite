b6f3e0fe21b2b8469c5b935af2c0d9704fac49fc
Elifio <ddtankgt@hotmail.com>
2024-04-03 17:00:34 -0300

    Create User In Db;

---
+++ ProjetoSite/Controllers/StudentsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoSite.Data;
+using ProjetoSite.Models;
+using ProjetoSite.Models.Entities;
+
+namespace ProjetoSite.Controllers
+    {
+    public class StudentsController : Controller
+        {
+            private readonly AplicativoDBContext dbContext;
+            public StudentsController(AplicativoDBContext dbContext)
+            {
+                this.dbContext = dbContext;
+            }
+
+        //End-Point para adicionar novos estudantes;
+        [HttpGet]
+        public IActionResult Add()
+            {
+            return View();
+            }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel) 
+            {
+
+            var student = new Student
+                {
+                    Name = viewModel.Name,
+                    Email = viewModel.Email,
+                    Phone = viewModel.Phone,
+                    Subscribed = viewModel.Subscribed,
+                };
+                
+                await dbContext.Students.AddAsync(student);
+                await dbContext.SaveChangesAsync();
+
+                return View();
+            }
+        }
+    }
--- ProjetoSite/Program.cs
+++ ProjetoSite/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AplicativoDBContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("StudentPortal")));
+options.UseSqlServer(builder.Configuration.GetConnectionString("StudentsPortal")));
 
 var app = builder.Build();
 
